6dffbe63c8d93c47b6f671a943f0c7f02b4928e5
Hoay <hoayyap@gmail.com>
2018-07-14 22:43:22 -0700

    test

--- Speedometer/Speedometer/AppDelegate.swift
+++ Speedometer/Speedometer/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Speedometer
 //
-//  Created by Mark DiFranco on 4/3/17.
+//  Created by Mark DiFranco on 4/3/17 test.
 //  Copyright © 2017 Mark DiFranco. All rights reserved.
 //
 
